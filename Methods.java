49dcc9b0505288f9053ff011289a99b53b7cdf5a
raz <48359006+rajroshansharma@users.noreply.github.com>
2022-04-14 19:47:56 +0530

    Simple implementation of methods.

---
+++ Methods.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void add_num(int a, int b){
+		System.out.println(a+b);
+	}
+	public static void sub_num(int a, int b){
+		System.out.println(a-b);
+	}
+	public static void mul_num(int a, int b){
+		System.out.println(a*b);
+	}
+	public static void divide_num(int a, int b){
+		System.out.println(a/b);
+	}
+	
+	public static void main(String[] args) {
+		add_num(6,8);
+		sub_num(6,8);
+		mul_num(6,8);
+		divide_num(6,8);
+	}
+}
